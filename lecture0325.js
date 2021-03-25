6ab4da8b7d26ed084fbc758a69f1480002728968
JH1ee <78770290+JH1ee@users.noreply.github.com>
2021-03-25 18:03:05 +0900

    Add files via upload

---
+++ lecture0325.js
@@ -0,0 +1,18 @@
+function return_test()
+{
+    return;
+    console.log("실행되지 않는 코드입니다.");
+}
+
+function print(msg)
+{
+    console.log("Print function IT");
+    console.log(msg);
+    console.log("print function out");
+}
+
+function sum(arg1, arg2)
+{
+    var result = arg1 + arg2;
+    return result;
+}
